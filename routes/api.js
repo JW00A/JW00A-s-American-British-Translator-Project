1930d3e8eb045e07921ed5d0b89f506c1b685306
JW00A <69700684+JW00A@users.noreply.github.com>
2025-06-01 09:38:09 -0400

    Add files via upload

---
+++ routes/api.js
@@ -0,0 +1,26 @@
+'use strict';
+
+const Translator = require('../components/translator.js');
+
+module.exports = function (app) {
+  
+  const translator = new Translator();
+
+  app.route('/api/translate')
+    .post((req, res) => {
+      const { text, locale } = req.body;
+
+      if (text == undefined || !locale) {
+        return res.json({ error: 'Required field(s) missing' });
+      }
+
+      if (req.body.text == "") return res.json({ error: "No text to translate" });
+
+      if (!['american-to-british', 'british-to-american'].
+            includes(locale)) {
+              return res.json({ error: 'Invalid value for locale field' });
+            }
+
+      res.json(translator.translate(text, locale));
+    });
+};
